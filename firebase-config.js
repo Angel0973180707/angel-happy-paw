bd6a313d995d7c482264c8766d7a3529868bf914
Angel0973180707 <a0973180707@gmail.com>
2026-01-18 17:29:51 +0800

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnSN6JM5boowTEPz_8v6FHKbHZM00FgTg",
+  authDomain: "happy-angel-pwa-bd814.firebaseapp.com",
+  projectId: "happy-angel-pwa-bd814",
+  storageBucket: "happy-angel-pwa-bd814.firebasestorage.app",
+  messagingSenderId: "766917719571",
+  appId: "1:766917719571:web:c86f0650a73a7bd8e9f1e6",
+  measurementId: "G-GY3NL68S7X"
+};
+export default firebaseConfig;
