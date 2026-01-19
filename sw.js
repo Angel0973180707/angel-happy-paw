faf4b10ca2b5bec6a92e36220fe2c075044b3106
Angel0973180707 <a0973180707@gmail.com>
2026-01-19 21:17:34 +0800

    Add service worker for PWA caching

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'angel-pwa-v2';
+const ASSETS = [
+  'index.html',
+  'assets/home.webp',
+  'assets/brain.webp',
+  'assets/soul.webp',
+  'assets/relation.webp',
+  'assets/story.webp',
+  'assets/tool.webp',
+  'assets/icon-192.png',
+  'assets/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
