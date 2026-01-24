b6e53b2a304f684e7f10c1424397b984acd70355
Angel0973180707 <a0973180707@gmail.com>
2026-01-24 10:14:22 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,2 +0,0 @@
-const C='af-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
